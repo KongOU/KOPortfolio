edc146514dac7484b3cdba3fbfe70ae7c708f8de
kongou <oukong84@gmail.com>
2019-07-11 23:46:14 +0700

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,3 +46,11 @@ end
 end
 
 puts "9 porfolio itmes created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
