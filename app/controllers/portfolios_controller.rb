872ecff8cda41127d5f09a5893a7001680aa9cac
kongou <oukong84@gmail.com>
2019-07-29 01:24:09 +0700

    Implemented form configuration for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,10 +9,11 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
@@ -31,7 +32,7 @@ class PortfoliosController < ApplicationController
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -18,5 +18,4 @@ class Portfolio < ApplicationRecord
     self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
     self.thumb_image ||= Placeholder.image_generator(height: "350", width: "200")
   end
-
 end
