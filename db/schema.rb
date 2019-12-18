cb6fe012a7a9381a672e9b03f3ae30ca7b1eb4c3
kongou <oukong84@gmail.com>
2019-12-18 16:08:46 +0700

    update gem bundle update --bundler

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_162453) do
+ActiveRecord::Schema.define(version: 2019_12_01_185152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
