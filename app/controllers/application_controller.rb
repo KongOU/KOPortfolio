c439a54d11bb35801ac38bd94b99391f16fc07e7
kongou <oukong84@gmail.com>
2020-01-14 22:05:26 +0700

    Refactored application controller to concern for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   include DeviseConcern
-
-  before_action :set_source
-
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end
+  include SourceConcern
 end
---
+++ app/controllers/concerns/source_concern.rb
@@ -0,0 +1,11 @@
+module SourceConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_source
+  end
+
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
+end
