b41ef6e5d7cf845128e2ce7445387151cfbe4ada
kongou <oukong84@gmail.com>
2020-01-16 23:36:21 +0700

    Implemented dynamic css class for nav-bar and partial with local data for portfolio form

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -30,6 +30,7 @@ class PortfoliosController < ApplicationController
 
   def edit
     @portfolio_item = Portfolio.find(params[:id])
+    3.times { @portfolio_item.technologies.build }
   end
 
   def update
